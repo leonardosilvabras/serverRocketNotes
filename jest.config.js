94a3442e8027d8a1b47988abb522201fe300201e
Leonardo Silva <103190146+leosilvafx@users.noreply.github.com>
2024-02-29 14:28:34 -0300

    test

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  bail: true,
+  coverageProvider: "v8",
+
+  testMatch:[
+    "**/*.spec.js"
+  ],
+}
---
+++ mytest.spec.js
@@ -0,0 +1,7 @@
+it("sum", () => {
+  const a = 2;
+  const b = 2;
+  const result = a + b;
+
+  expect(result).toEqual(4);
+})
