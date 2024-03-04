b69bfa3f64182c4b5ee14dce200eec1cbe1d2a06
Leonardo Silva <103190146+leosilvafx@users.noreply.github.com>
2024-03-04 08:55:29 -0300

    UserCreateServices

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -8,18 +8,6 @@ class UsersController {
   async create(request, response) {
     const { name, email, password } = request.body;
 
-    const userRepository = new UserRepository();
-
-    const checkUserExists = await userRepository.findByEmail(email);
-    
-    if (checkUserExists) {
-      throw new AppError("Este e-mail já está em uso.");
-    }
-
-    const hashedPassword = await hash(password, 8);
-
-    await userRepository.create({ name, email, password: hashedPassword });
-
     return response.status(201).json();
   }
 
---
+++ src/services/UserCreateServices.js
@@ -0,0 +1,19 @@
+class UserCreateServices {
+  constructor(userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  async execute({ name, email, password }) {
+    const checkUserExists = await this.userRepository.findByEmail(email);
+
+    if (checkUserExists) {
+      throw new AppError("Este e-mail já está em uso.");
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    await this.userRepository.create({ name, email, password: hashedPassword });
+  }
+}
+
+module.exports = UserCreateServices;
